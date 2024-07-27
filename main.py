9b60ba1e4c577c138aa5903e70356f48137492d1
bushishiba114 <161920757+bushishiba114@users.noreply.github.com>
2024-07-27 11:02:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import requests
 from hashlib import md5
 import math
 import time
-
+import os
 
 
 secretKey = "HIOWEHGW23-56803" 
-
+os.mkdir('l')
 for i in range(9):
     p=time.time() 
     p=math.floor(p) 
@@ -21,7 +21,7 @@ for i in range(9):
     params1={'timestamp':p,'token':token}
     print(params1)
     x = requests.get('https://box.fiime.cn/random/random.php',params={'timestamp':p,'token':token},stream=True)
-    m=open(pp+".png",'wb')
+    m=open('l//'+pp+".png",'wb')
     m.write(x.content)
     m.close()
     time.sleep(5)
